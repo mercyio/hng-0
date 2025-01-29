3c8e3bbaad660fdf18a9f78d620b38ec27771c6a
mercy vincent <mercydanke@gmail.com>
2025-01-30 00:39:45 +0100

    Initial commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getInfo() {
+  async getUserInfo() {
     const data = await this.appService.getUserInfo();
     return {
       statusCode: HttpStatus.OK,
