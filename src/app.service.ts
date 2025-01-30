4fe3e5a131147af9d3b96b32780ffecf01b1725b
mercy vincent <mercydanke@gmail.com>
2025-01-30 02:58:23 +0100

    updated current time format

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,12 +4,11 @@ import { Injectable } from '@nestjs/common';
 export class AppService {
   async getUserInfo() {
     const now = new Date();
-    const date = now.toISOString().split('T')[0];
     const time = `${now.getUTCHours()}:${now.getUTCMinutes()}:${now.getUTCSeconds()}`;
 
     return {
       email: 'mercydanke@gmail.com',
-      current_datetime: `${date}T${time}Z`,
+      current_datetime: time,
       github_url: 'https://github.com/mercyio/hng-0',
     };
   }
